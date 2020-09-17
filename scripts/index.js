7d30cd178055918f68cb73bc7993e99c6ba02f7a
tf153 <43797501+tf153@users.noreply.github.com>
2020-09-17 15:04:02 +0530

    rj
    homepage
    homepage styling
    homepage script

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+{
+	var modal1 = document.getElementById('id01');
+	var modal2 = document.getElementById('id02');
+	window.onclick = function(event) {
+		if (event.target == modal1 || event.target == modal2) {
+			modal1.style.display = "none";
+			modal2.style.display = "none";
+		}
+	}
 }
